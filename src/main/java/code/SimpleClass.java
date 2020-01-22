f9d3b1b5516e7a0cc3cd0f731cdda936a7740aef
Gordon Hutchison <Gordon.Hutchison@gmail.com>
2020-01-22 16:06:37 +0000

    initial local files
    Signed-off-by: Gordon Hutchison <Gordon.Hutchison@gmail.com>

---
+++ src/main/java/code/SimpleClass.java
@@ -0,0 +1,10 @@
+package example;
+
+public class SimpleClass {
+    String name;
+    // tag::setName[]
+    public setName(String name) {
+        this.name = name;
+    }
+    // end::setName[]
+}
